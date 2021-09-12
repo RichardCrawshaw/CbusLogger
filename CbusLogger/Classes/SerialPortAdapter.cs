813575db13896049158ef8ce0de2c735b821d194
Richard Crawshaw <richardcrawshaw41@gmail.com>
2021-09-12 15:32:50 +1000

    Add project files.

---
+++ CbusLogger/Classes/SerialPortAdapter.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.IO;
+using System.IO.Ports;
+using NLog;
+
+namespace Crowswood.CbusLogger
+{
+    /// <summary>
+    /// Adapter class for <see cref="System.IO.Ports.SerialPort"/> that fixes
+    /// a number of its issues. See this blog post by Ben Voigt:
+    /// https://www.sparxeng.com/blog/software/must-use-net-system-io-ports-serialport
+    /// </summary>
+    internal class SerialPortAdapter
+    {
+        #region Variables
+
+        const int READ_BUFFER_SIZE = 64;
+
+        private static readonly Logger logger = LogManager.GetCurrentClassLogger();
+
+        private readonly SerialPort serialPort;
+
+        #endregion
+
+        #region Properties
+
+        public int BaudRate => this.serialPort.BaudRate;
+
+        public uint BufferSize { get; set; } = READ_BUFFER_SIZE;
+
+        public int DataBits => this.serialPort.DataBits;
+
+        public bool IsOpen => this.serialPort.IsOpen;
+
+        public Parity Parity => this.serialPort.Parity;
+
+        public string PortName => this.serialPort.PortName;
+
+        public StopBits StopBits => this.serialPort.StopBits;
+
+        public Stream Stream => this.serialPort?.BaseStream;
+
+        #endregion
+
+        #region Constructors
+
+        public SerialPortAdapter(string portName, int baudRate, Parity parity, int dataBits, StopBits stopBits)
+        {
+            logger.Trace(() => $"Creating {portName} ({baudRate} {parity} {dataBits} {stopBits}).");
+
+            this.serialPort = GetSerialPort(portName, baudRate, parity, dataBits, stopBits);
+            if (this.serialPort == null)
+                throw new IOException($"Failed to create port: {portName} ({baudRate} {parity} {dataBits} {stopBits}).");
+        }
+
+        #endregion
+
+        #region Methods
+
+        public static Parity ConvertParity(string parity) => Enum.TryParse<Parity>(parity, out var result) ? result : default;
+
+        public static StopBits ConvertStopBits(string stopBits) => Enum.TryParse<StopBits>(stopBits, out var result) ? result : default;
+
+        public void Close() => this.serialPort.Close();
+
+        public void Open()
+        {
+            logger.Trace(() => $"Opening serial port: {this.serialPort.PortName}.");
+
+            this.serialPort.Open();
+            if (!this.serialPort.IsOpen) return;
+
+            ReadIncomingData(this.serialPort);
+        }
+
+        public void Send(byte[] buffer) => this.serialPort.Write(buffer, 0, buffer.Length);
+
+        public void Write(string text) => this.serialPort.Write(text);
+
+        #endregion
+
+        #region Events
+
+        public event EventHandler<ReceivedSerialDataEventArgs> ReceivedSerialData;
+
+        public event EventHandler<SerialPortErrorEventArgs> SerialPortError;
+
+        #endregion
+
+        #region Support routines
+
+        private static SerialPort GetSerialPort(string portName, int baudRate, Parity parity, int dataBits, StopBits stopBits)
+        {
+            var serialPort = new SerialPort(portName, baudRate, parity, dataBits, stopBits);
+
+            return serialPort;
+        }
+
+        private void ReadIncomingData(SerialPort serialPort)
+        {
+            byte[] buffer = new byte[this.BufferSize];
+            void kickoffRead(Stream stream)
+            {
+                stream.BeginRead(buffer, 0, buffer.Length,
+                    delegate (IAsyncResult result)
+                    {
+                        try
+                        {
+                            if (serialPort.IsOpen)
+                            {
+                                var actualLength = stream.EndRead(result);
+                                var received = new byte[actualLength];
+                                Buffer.BlockCopy(buffer, 0, received, 0, actualLength);
+                                this.ReceivedSerialData?.Invoke(serialPort,
+                                    new ReceivedSerialDataEventArgs
+                                    {
+                                        ReceivedSerialData = received,
+                                    });
+                            }
+                        }
+                        catch (IOException ex)
+                        {
+                            if (serialPort.IsOpen)
+                                this.SerialPortError?.Invoke(this,
+                                    new SerialPortErrorEventArgs
+                                    {
+                                        Exception = ex,
+                                    });
+                        }
+                        if (serialPort.IsOpen)
+                            kickoffRead(stream);
+                    },
+                    null);
+            }
+
+            kickoffRead(serialPort.BaseStream);
+
+            logger.Trace(() => $"{this.serialPort.PortName} waiting for incoming data.");
+        }
+
+        #endregion
+    }
+
+    public class ReceivedSerialDataEventArgs : EventArgs
+    {
+        public byte[] ReceivedSerialData { get; set; }
+    }
+
+    public class SerialPortErrorEventArgs : EventArgs
+    {
+        public Exception Exception { get; set; }
+    }
+}
---
+++ CbusLogger/Public/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Threading;
+using NLog;
+using NLog.Targets;
+using NLog.Targets.Wrappers;
+
+namespace Crowswood.CbusLogger
+{
+    class Program
+    {
+        private static readonly Logger logger = LogManager.GetCurrentClassLogger();
+
+        private static readonly AutoResetEvent autoResetEvent = new(false);
+
+        private static int comPortNumber;
+        //private static bool interpretMessages = false;
+
+        static int Main(string[] args)
+        {
+            // Args
+            // COM Port /c<int> eg /c1 to use COM1
+            // List ports /d
+            if (!GetArgs(args))
+            {
+                Console.WriteLine("Press [Enter] to exit.");
+                Console.ReadLine();
+                return 0;
+            }
+
+            if (comPortNumber < 1)
+            {
+                logger.Fatal(() => $"Invalid COM Port number: {comPortNumber}.");
+                return 1;
+            }
+
+            Console.WriteLine($"Using COM{comPortNumber}");
+
+            var target = LogManager.Configuration.FindTargetByName("file");
+            while ((target != null) && (target is WrapperTargetBase))
+                target = (target as WrapperTargetBase).WrappedTarget;
+            if (target is FileTarget fileTarget)
+            {
+                var logEventInfo = new LogEventInfo { TimeStamp = DateTime.Now, };
+                var filename = fileTarget.FileName.Render(logEventInfo);
+                filename = filename.Replace(@"\/", @"\");
+                filename = filename.Replace("/", @"\");
+                Console.WriteLine($"Logging to {filename}");
+            }
+
+            ThreadPool.QueueUserWorkItem(LogIncomingMessages);
+            Console.WriteLine("Logging incoming messages.");
+            Console.WriteLine("Press [Enter] to exit.");
+
+            Console.ReadLine();
+
+            autoResetEvent.Set();
+
+            return 0;
+        }
+
+        private static bool GetArgs(string[] args)
+        {
+            comPortNumber = 1;
+
+            foreach(var arg in args)
+            {
+                if (arg[0] != '/') continue;
+                switch (arg[1])
+                {
+                    case 'c':
+                        _ = int.TryParse(arg[2..], out comPortNumber);
+                        break;
+                    case 'd':
+                        var ports = SerialPort.GetPortNames();
+                        Console.WriteLine($"{ports.Length} COM ports found:");
+                        foreach (var port in ports)
+                            Console.WriteLine(port);
+                        return false;
+                    //case 'i':
+                    //    interpretMessages = true;
+                    //    break;
+                    case '?':
+                        Console.WriteLine("/cn\tSets COM Port n, where n > 0");
+                        Console.WriteLine("/d\tDisplays all the available ports");
+                        //Console.WriteLine("/i\tInterprets messages");
+                        Console.WriteLine("/?\tDisplays this help message.");
+                        return false;
+                }
+            }
+
+            return true;
+        }
+
+        private static void LogIncomingMessages(object state)
+        {
+            var portName = $"COM{comPortNumber}";
+            var baudRate = 115200;
+            var parity = Parity.None;
+            var dataBits = 8;
+            var stopBits = StopBits.One;
+            var serial = new SerialPortAdapter(portName, baudRate, parity, dataBits, stopBits);
+            serial.ReceivedSerialData += Serial_ReceivedSerialData;
+
+            serial.Open();
+
+            autoResetEvent.WaitOne();
+
+            serial.Close();
+
+            serial.ReceivedSerialData -= Serial_ReceivedSerialData;
+        }
+
+        private static void Serial_ReceivedSerialData(object sender, ReceivedSerialDataEventArgs e)
+        {
+            var message = new string(e.ReceivedSerialData.Select(d => (char)d).ToArray());
+            logger.Info(() => message);
+        }
+    }
+}
